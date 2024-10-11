5800a10d2ed9d3755c1dfc25af4f81745ff59673
Balu1907 <balu562002@gmail.com>
2024-10-11 12:34:36 +0530

    remove s3

--- next.config.js
+++ next.config.js
@@ -7,10 +7,10 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/s/:snapshotId',
-        destination: '/api/sameorigin/:snapshotId',
+        source: "/s/:snapshotId",
+        destination: "/api/sameorigin/:snapshotId",
       },
-      { source: '/r/:snapshotId', destination: '/api/share/:snapshotId' },
+      { source: "/r/:snapshotId", destination: "/api/share/:snapshotId" },
     ];
   },
 };
